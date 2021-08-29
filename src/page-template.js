d29f44c4f85ef28b070c2b5b329523f0b643d5b3
Brad Smith <smithfamily42@gmail.com>
2021-08-29 13:18:28 -0700

    updated template for engineer fix

--- src/page-template.js
+++ src/page-template.js
@@ -32,7 +32,7 @@ const generateTeam = team => {
         <ul class="list-group">
             <li class="list-group-item">ID: ${engineer.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}"}${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">$engineer.getGithub()} </a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()} </a></li>
         </ul>
         </div>
         </div>
